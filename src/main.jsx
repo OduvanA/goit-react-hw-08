ff4202e440c0de295e886fede8eb223141c8fd5a
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-08-04 00:36:12 -0500

    fixing a bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize';
+import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,6 +15,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
  
