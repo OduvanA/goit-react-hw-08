d0b540bc33900bcc6755856ee6f487d373e6ae5e
OduvanA <145233058+OduvanA@users.noreply.github.com>
2024-08-04 18:46:01 -0500

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -8,7 +8,7 @@ export default function UserMenu() {
 
   return (
     <div>
-      <p>Welcome, {user.name}</p>
+      <p>Welcome, {user.name}!</p>
       <button type='button' onClick={() => dispatch(logout())}>Logout</button>
     </div>
   );
